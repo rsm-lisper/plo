0bd341ea4aed2e432f93cba8e54501b4ed1f9bb5
Rafał -rsm- Marek <rsm@lisper.pl>
2018-10-29 02:48:56 +0100

    true i false, powrót do klasycznego formatowania (niezaskakiwanie)

--- plo.php
+++ plo.php
@@ -2,24 +2,80 @@
 
 namespace plo;
 
-function ror (... $args) { return
-              empty ($args) ? false :
-              ($args[0] ? $args[0] :
-               ror (... array_shift ($args)) ;}
+/**
+ * Sprawdza czy argument jest prawdą (true).
+ *
+ * @param mixed $arg Argument do sprawdzenia.
+ * @return bool
+ */
+function truep ($arg) {
+    return $arg == true;
+}
 
-function rand (... $args) { return
-              empty ($args) ? true :
-              ($args[0] ? rand (... array_shift ($args)) :
-               $args[0]) ;}
+/**
+ * Sprawdza czy argument jest fałszem (false).
+ *
+ * @param mixed $arg Argument do sprawdzenia.
+ * @return bool
+ */
+function falsep ($arg) {
+    return $arg == false;
+}
 
-function equalp ($a, $b) { return
-        $a == $b ;}
 
-function sequalp ($a, $b) { return
-        $a === $b ;}
+/**
+ * Sprawdza ściśle (strict) czy argument jest prawdą (true).
+ *
+ * @param mixed $arg Argument do sprawdzenia.
+ * @return bool
+ */
+function struep ($arg) {
+    return $arg === true;
+}
 
-function nullp ($a) { return
-        equalp ($a, null) ;}
 
-function snullp ($a) { return
-        sequal ($a, null) ;}
+/**
+ * Sprawdza ściśle (strict) czy argument jest fałszem (false).
+ *
+ * @param mixed $arg Argument do sprawdzenia.
+ * @return bool
+ */
+function sfalsep ($arg) {
+    return $arg === false;
+}
+
+
+function ror (...$args) {
+    return
+        (empty($args) ? false :
+         ($args[0] ? $args[0] :
+          ror(...array_shift($args))));
+}
+
+
+function rand (...$args) {
+    return
+        (empty($args) ? true :
+         ($args[0] ? rand(...array_shift($args)) :
+          $args[0]));
+}
+
+
+function equalp ($a, $b) {
+    return $a == $b;
+}
+
+
+function sequalp ($a, $b) {
+    return $a === $b;
+}
+
+
+function nullp ($a) {
+    return equalp($a, null);
+}
+
+
+function snullp ($a) {
+    return sequal($a, null);
+}
