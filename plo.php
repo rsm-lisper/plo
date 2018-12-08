35149d027af045b2b349f5a3b778c6c2427115d4
Rafał -rsm- Marek <rsm@lisper.pl>
2018-12-08 23:51:08 +0100

    w trakcie...

--- plo.php
+++ plo.php
@@ -1,64 +1,88 @@
 <?php
 
-namespace plo;
+namespace plo ;
 
-/**
- * Sprawdza czy argument jest prawdą (true).
- *
- * @param mixed $arg Argument do sprawdzenia.
- * @return bool
- */
-function truep ($arg) {
-    return $arg == true;
-}
+require 'lib/pva/pva.php' ;
 
-/**
- * Sprawdza czy argument jest fałszem (false).
- *
- * @param mixed $arg Argument do sprawdzenia.
- * @return bool
- */
-function falsep ($arg) {
-    return $arg == false;
-}
+function array_cut_first ($array) {
+    array_shift ($array) ;
+    return $array ;}
+
+
+function simple_sfalsep ($arg) { return
+        $arg === false ;}
 
 
 /**
- * Sprawdza ściśle (strict) czy argument jest prawdą (true).
+ * Sprawdza ściśle (strict) czy każdy argument jest fałszem (false).
  *
- * @param mixed $arg Argument do sprawdzenia.
+ * W wersji ścisłej wartość logiczną fałsz (false) ma wyłącznie bool false.
+ * Wywołane bez argumentów zwraca null;
+ *
+ * @param mixed $args Argumenty do sprawdzenia.
  * @return bool
  */
-function struep ($arg) {
-    return $arg === true;
-}
+function sfalsep (...$args) { return
+        (count ($args) === 0 ? null :
+         (count ($args) === 1 ? simple_sfalsep ($args[0]) :
+          (simple_sfalsep ($args[0]) ? sfalsep (...array_cut_first ($args)) :
+           false)) ;}
 
 
 /**
- * Sprawdza ściśle (strict) czy argument jest fałszem (false).
+ * Sprawdza ściśle (strict) czy każdy argument jest prawdą (true).
  *
- * @param mixed $arg Argument do sprawdzenia.
+ * W wersji ścisłej wartość logiczną prawda (true) ma wszystko poza bool false.
+ *
+ * @param mixed $args Argumenty do sprawdzenia.
  * @return bool
  */
-function sfalsep ($arg) {
-    return $arg === false;
-}
+function struep (...$args) { return
+        ! sfalsep (...$args) ;}
 
 
-function ror (...$args) {
-    return
-        (empty($args) ? false :
+function simple_falsep ($arg) { return
+        simple_sfalsep ($arg) ||
+        $arg === [] ||
+        $arg === 0.0 ||
+        $arg === 0 ||
+        $arg === "" ||
+        $arg === null ;}
+
+
+/**
+ * Sprawdza czy każdy argument jest fałszem (false).
+ *
+ * @param mixed $args Argumenty do sprawdzenia.
+ * @return bool
+ */
+function falsep (...$args) { return
+        (count ($args) === 0 ? null :
+         (count ($args) === 1 ? simple_falsep ($args[0]) :
+          (simple_falsep ($args[0]) ? falsep (...array_cut_first ($args)) :
+           false)) ;}
+
+
+/**
+ * Sprawdza czy każdy argument jest prawdą (true).
+ *
+ * @param mixed $args Argumenty do sprawdzenia.
+ * @return bool
+ */
+function truep (...$args) { return
+        ! falsep (...$args) ;}
+
+
+function orp (...$args) { return
+        (count ($args) === 0 ? false :
          ($args[0] ? $args[0] :
-          ror(...array_shift($args))));
-}
+          orp (...array_shift ($args)))) ;}
 
 
-function rand (...$args) {
-    return
-        (empty($args) ? true :
-         ($args[0] ? rand(...array_shift($args)) :
-          $args[0]));
-}
+function andp (...$args) { return
+        (empty ($args) ? true :
+         ($args[0] ? andp (...array_shift ($args)) :
+          $args[0])) ;}
 
 
 function equalp ($a, $b) {
