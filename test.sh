f50cec7ca9f621e4b14c3fc5633bcb404ef89935
Rafał -rsm- Marek <rsm@lisper.pl>
2019-01-16 19:23:28 +0100

    przeniesienie testów do podkatalogu

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-php test.php
-ex=$?
-if (( $ex == 0)); then echo "TEST OK"; fi
-exit $ex
--- test/test.php
+++ test/test.php
@@ -2,12 +2,12 @@
 
 namespace plo ;
 
-require 'lib/ppt/ppt.php' ;
-require 'plo.php' ;
+require 'ppt/ppt.php' ;
+require '../plo.php' ;
 
 $plo_test_specs = [
     ['single strict false p',
-     ['bool false', single_sfalsep (false), true],
+     ['bool false', \plo\single_sfalsep (false), true],
     ]];
 
 \ppt\exit_nicely (\ppt\test_all ($plo_test_specs));
